b5c8dad042ae099bdf1823b826a64e66e207d65a
lealobanov <lea.lobanov@icloud.com>
2023-10-17 21:14:36 +0900

    Debugging dates

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
 export const purchaseNftOnMarketplace = {
   slug: recipe,
   title: "Purchase NFT on Marketplace",
-  createdAt: Date(2022, 3, 1),
+  createdAt: new Date(2022, 3, 1),
   author: "Flow Blockchain",
   playgroundLink:
     "https://play.onflow.org/1d11f838-fc0e-4e7f-86e3-6d1a5a1098e3?type=tx&id=7c4fa7ca-5770-4dd4-97d3-ed575f208d22&storage=none",
