5c03f720ebdc8f4db7097bd6ead32dc3378bf29c
Lea Lobanov <lea.lobanov@icloud.com>
2024-12-21 14:43:00 +0400

    Simplify recipe fields

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 // Pass the repo name
 const recipe = "purchase-nft-on-marketplace";
 
-//Generate paths of each code file to render
-const contractPath = `${recipe}/cadence/contract.cdc`;
-const transactionPath = `${recipe}/cadence/transaction.cdc`;
-
-//Generate paths of each explanation file to render
-const smartContractExplanationPath = `${recipe}/explanations/contract.txt`;
-const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
-
 export const purchaseNftOnMarketplace = {
   slug: recipe,
   title: "Purchase NFT on Marketplace",
@@ -17,10 +9,6 @@ export const purchaseNftOnMarketplace = {
   playgroundLink:
     "https://play.onflow.org/1d11f838-fc0e-4e7f-86e3-6d1a5a1098e3?type=tx&id=7c4fa7ca-5770-4dd4-97d3-ed575f208d22&storage=none",
   excerpt: "Buy an NFT from a marketplace.",
-  smartContractCode: contractPath,
-  smartContractExplanation: smartContractExplanationPath,
-  transactionCode: transactionPath,
-  transactionExplanation: transactionExplanationPath,
   filters: {
     difficulty: "intermediate",
   },
